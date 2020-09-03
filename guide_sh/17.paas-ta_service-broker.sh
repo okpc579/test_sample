55333f7f6a0ae31da1ec9c98c1d10a5d84996966
okpc579 <ski579579@naver.com>
2020-09-03 01:09:24 +0000

    secend

--- guide_sh/17.paas-ta_service-broker.sh
+++ guide_sh/17.paas-ta_service-broker.sh
@@ -3,6 +3,6 @@
 source variables.sh
 
 cf login -a https://api.${PaaS-TA_Domain} --skip-ssl-validation -u ${PaaS-TA_Admin_ID} -p ${PaaS-TA_Admin_Paasword} -o ${Create_Org_Name} -s ${Create_Space_Name}
-cf push -f ${Sample_Path}/etc/bookstore_service_broker/manifest.yml
+cf push -f ${Sample_Path}/etc/bookstore_service_broker/deploy/cloudfoundry/manifest.yml
 cf create-service-broker bookstore admin supersecret https://bookstore-service-broker.${PaaS-TA_Domain}
 cf service-brokers
