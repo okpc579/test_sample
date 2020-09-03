7138d2aa8ef00384b0f3df86839358fc911733d4
okpc579 <ski579579@naver.com>
2020-09-03 01:38:41 +0000

    4

--- guide_sh/04.bosh_runtime-config.sh
+++ guide_sh/04.bosh_runtime-config.sh
@@ -2,5 +2,5 @@
 
 source variables.sh
 
-bosh -e ${BOSH_Director_Name} update-runtime-config ${BOSH_Path}/runtime-config/dns.yml
+bosh -e ${BOSH_Director_Name} update-runtime-config ${BOSH_Path}/runtime-configs/dns.yml
 bosh -e ${BOSH_Director_Name} runtime-config
